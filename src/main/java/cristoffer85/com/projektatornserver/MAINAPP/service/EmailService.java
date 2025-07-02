6df108f7497910ffabb6f4961f728868206a8b4a
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-07-02 13:17:22 +0200

    project email notification added

--- src/main/java/cristoffer85/com/projektatornserver/MAINAPP/service/EmailService.java
+++ src/main/java/cristoffer85/com/projektatornserver/MAINAPP/service/EmailService.java
@@ -22,4 +22,16 @@ public class EmailService {
         message.setText("Click the following link to reset your password: " + resetLink);
         mailSender.send(message);
     }
+
+        public void sendProjectNotificationEmail(String to, String fromUser, String projectIdea) {
+        String subject = "New Project Shared With You";
+        String text = "Hi!\n\n" +
+                    fromUser + " has shared a new project with you: \"" + projectIdea + "\".\n" +
+                    "Log in to your account to view the details.";
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
 }
--- src/main/java/cristoffer85/com/projektatornserver/MAINAPP/service/ProjectsInProgressService.java
+++ src/main/java/cristoffer85/com/projektatornserver/MAINAPP/service/ProjectsInProgressService.java
@@ -2,6 +2,8 @@ package cristoffer85.com.projektatornserver.MAINAPP.service;
 
 import cristoffer85.com.projektatornserver.MAINAPP.model.ProjectsInProgress;
 import cristoffer85.com.projektatornserver.MAINAPP.repository.ProjectsInProgressRepository;
+import cristoffer85.com.projektatornserver.MAINAPP.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,29 @@ public class ProjectsInProgressService {
     @Autowired
     private ProjectsInProgressRepository repository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EmailService emailService;
+
     public List<ProjectsInProgress> getProjectsForUser(String username) {
         return repository.findByOwnerOrFriend(username, username);
     }
 
     public ProjectsInProgress addProject(ProjectsInProgress project) {
-        return repository.save(project);
+        ProjectsInProgress saved = repository.save(project);
+
+        // Find friend's email
+        userRepository.findByUsername(project.getFriend()).ifPresent(friendUser -> {
+            emailService.sendProjectNotificationEmail(
+                friendUser.getEmail(),
+                project.getOwner(),
+                project.getIdea()
+            );
+        });
+
+        return saved;
     }
 
     public void removeProject(String id) {
